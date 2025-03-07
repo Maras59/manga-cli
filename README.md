df4a99b6c782dbc87bed1be9feb3acbf4f91398e
Maras59 <marcosespino103@gmail.com>
2025-03-06 16:55:56 -0800

    Change project scope

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # manga-cli
-Command line tool that renders manga panels in ascii
+Command line tool that renders manga panels in ascii.
 
 Requires `apt-get install libmagickwand-dev`
---
+++ manga-cli.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+// #include <curses.h>
+#include <locale.h>
+// #include <panel.h>
+#include <getopt.h>
+#include <time.h>
+#include <string.h>
+#include <wchar.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
+int main () {
+    printf("Hello World!\n");
+    return 0;
+}
